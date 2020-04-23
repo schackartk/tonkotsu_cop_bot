14dfdf4fbf6d3003b68db8e101d0e8e20c97849d
Kenny's Spectre <schackartk@email.arizona.edu>
2020-04-22 22:46:02 -0700

    add example strings

--- test.py
+++ test.py
@@ -42,3 +42,22 @@ def test_bad_input():
     assert rv > 0
     assert out == 'No file "{}" found.'.format(bad_file)
     
+def test_label():
+    """label"""
+    
+    test = [('Nothing better than a good bowl of tonkatsu', True),
+            ('I did it!!! I made Tonkatsu Chcachu Ramen!', True),
+            ('Tonkatsu Ramen', True),
+            ('Absolutely nailed my Tonkatsu. Finally!', True),
+            ('Kakuni Tonkatsu Ramen', True),
+            ('tonkatsu from hell - Samurai Noodles in Seattle', True),
+            ('Spicy Tonkatsu Ramen from Atlanta, GA. Egg could\'ve used a bit more work but the broth was amazing', True),
+            ('Tonkatsu ramen from Ramen-San in Chicago', True),
+            ('Awesome pork tonkatcu ramen at Haiku Tokyo', True),
+            ('Spicy Tonkatsu', True),
+            ('Tonkatsu Black (ramen from my local ramen shop). Chashu, black fungus, green onions, egg, and garlic oil. Absolutely heavenly!', True),
+            ('This is officially my favorite kind of ramen broth. Spicy tonkatsu. I got it level 3 spicy. The broth is so creamy and fatty.', True),
+            ('Tonkatsu Tonkotsu ramen', False),
+            ('Can we have a tonkatsu/tonkotsu correction-bot?', False),
+            ('Tonkatsu vs. Tonkotsu - a Friendly PSA', False)
+            ]
