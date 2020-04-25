a04889d99841bba65370bb9d3f0b6f2fa524733c
Kenny's Spectre <schackartk@email.arizona.edu>
2020-04-24 21:20:07 -0700

    begin incporating ML model into bot - nonfunctional

--- bot.py
+++ bot.py
@@ -5,8 +5,10 @@ Date   : 22 April 2020
 """
 
 import argparse
+import bayes # My model file
 import config # log in information file
 import os
+import pickle
 import praw
 import re
 import sys
@@ -18,7 +20,7 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Run the Tonkotsu Police Bot',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
+    
     parser.add_argument(
         '-p',
         '--posts',
@@ -34,6 +36,14 @@ def get_args():
         metavar='FILE',
         type=str,
         default='data/deleted.txt')
+    
+    parser.add_argument(
+        '-m',
+        '--model',
+        help='Bodel for classifying titles',
+        metavar='PKL',
+        type=str,
+        default='MNB_model.pkl')
 
     return parser.parse_args()
 
@@ -86,9 +96,25 @@ def bot_login():
     return r
 
 # --------------------------------------------------
-def investigate(r, history, id_file):
+def predict(text, model_file):
+    """Use previously trained model to classify title text"""
+    
+    with open(model_file, 'rb') as file:
+        model, x_test, y_test, model_accuracy = pickle.load(file)
+        
+    if model_accuracy != model.score(x_test, y_test):
+        warn('Saved and test model accuracy do not match')
+    
+    text_features = bayes.get_features(text)
+    prediction = model.predict(text_features)
+    
+    return prediction
+
+# --------------------------------------------------
+def investigate(r, history, id_file, model_file):
     """Look for tonkotsu misspelling"""
-    katsu_count = 0
+    katsu_count = 0 # Number of instances corrected
+    
     msg = '''^(Beep boop, I am a robot)  
     Did you happen to mean 
     [tonkotsu](https://en.wikipedia.org/wiki/Tonkotsu_ramen) instead of 
@@ -102,14 +128,24 @@ def investigate(r, history, id_file):
     print('Scanning... ')
     posts = r.subreddit('test').new(limit=25)
     for post in posts:
-        if 'tonkatsu' in post.title.lower() and post.id not in history['post_id']: 
-        # See if it is likely not a mistake
-        # Check if user corrected it
+        
+        post_title = post.title.lower()
+        
+        if 'tonkatsu' in post_title and post.id not in history['post_id']: 
             print('Tonkatsu found in post: {}.'.format(post.id))
-            katsu_count += 1
-            with open(id_file, 'a') as fh:
-                print(post.id, file=fh)
-            post.reply(msg)
+            print('Post title: {}'.post.title)
+            
+            if predict(post_title, model_file):
+                print('Model predicted mistake spelling')
+                # Check if user corrected it
+                katsu_count += 1
+                with open(id_file, 'a') as fh:
+                    print(post.id, file=fh)
+                post.reply(msg)
+                # Need to add in message bot's acct
+            else:
+                print('Model predicted correct spelling')
+                # Need to add in message bot's acct
             
     print('Done scanning.')
     print('Commented on {} posts'.format(katsu_count))
@@ -135,18 +171,24 @@ def purge(r, history, del_file):
     
 # --------------------------------------------------
 def main():
-    """The good stuff"""    
+    """The good stuff""" 
+    
+    # Retrieve command-line arguments from argparse
     args = get_args()
     id_file = args.posts
     del_file = args.deleted
+    model_file = args.model
     
+    # Get history from files
+    # Make 'history' dictionary with keys: post_id, deleted
     history = get_history(id_file, del_file)
       
+    # Log in to reddit
     r = bot_login()
     
 #    while True:
 #        try:        
-    investigate(r, history, id_file)
+    investigate(r, history, id_file, model)
     purge(r, history, del_file)
 #    time.sleep(30)
 #        except:
