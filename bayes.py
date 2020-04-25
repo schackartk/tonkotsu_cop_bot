d8ace94d48c53ba49835b3548b0d4ab259d28540
schackartk <schackartk@email.arizona.edu>
2020-04-24 19:29:22 -0700

    incorporate pickling

--- bayes.py
+++ bayes.py
@@ -8,6 +8,7 @@ Date   : 23 April 2020
 import matplotlib.pyplot as plt # Generating graphical confusion matrix
 import pandas as pd
 import os
+import pickle
 import re # Regular expressions
 import seaborn as sn # Generating heatmap
 
@@ -41,7 +42,7 @@ def get_features(titles):
     return features
 
 # --------------------------------------------------
-def generate_model(X_train, X_test, y_train):
+def MNB_model_generate(X_train, X_test, y_train): # Multinomial Naive Bayes' Model
     """Train Naive Bayes model"""  
     naive_model = MultinomialNB()
     classifier = naive_model.fit(X_train, y_train)
@@ -66,17 +67,28 @@ def main():
     
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
     
-    trained_model = generate_model(x_train, x_test, y_train)
+    MNB_model = MNB_model_generate(x_train, x_test, y_train)
         
-    prediction = trained_model.predict(x_test)
+    MNB_prediction = MNB_model.predict(x_test)
     
-    cm = confusion_matrix(prediction, y_test)
+    MNB_confusion = confusion_matrix(MNB_prediction, y_test)
     plt.figure(figsize = (10,7))
-    sn.heatmap(cm, annot=True)
+    sn.heatmap(MNB_confusion, annot=True)
     
-    accuracy = cm.trace()/cm.sum()
-    print('Accuracy is: {}'.format(accuracy))
+    MNB_accuracy = MNB_confusion.trace()/MNB_confusion.sum()
+    MNB_accuracy = MNB_model.score(x_test, y_test)
+    print('Accuracy is: {}'.format(MNB_accuracy))
     
+    pkl_filename = 'MNB_model.pkl'
+    
+    with open(pkl_filename, 'wb') as file:
+        pickle.dump(MNB_model, file)
+        
+    with open(pkl_filename, 'rb') as file:
+        pickle_model = pickle.load(file)
+        
+    score = pickle_model.score(x_test, y_test)
+    print('Test score: {}'.format(100*score))        
 # --------------------------------------------------
 if __name__ == '__main__':
     main()    
