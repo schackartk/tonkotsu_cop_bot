b373b78f699b8a8c1c31ccddfb93f916b5c2b1c8
Kenny's Spectre <schackartk@email.arizona.edu>
2020-05-12 21:57:51 -0700

    update file structure defaults and break off functions

--- bayes.py
+++ bayes.py
@@ -43,9 +43,36 @@ def get_args():
         help='Name of model output (pickle)',
         metavar='FILE',
         type=str,
-        default='MNB_model.pkl')
+        default='data/model.pkl')
+    
+    parser.add_argument(
+        '-t',
+        '--test_out',
+        help='Test data output file',
+        metavar='FILE',
+        type=str,
+        default='data/test_data.txt')
+    
+    parser.add_argument(
+        '-s',
+        '--test_split',
+        help='Test data split ratio',
+        metavar='FLOAT',
+        type=str,
+        default=0.2)
     
     return parser.parse_args()
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
 
 # --------------------------------------------------
 def clean_title(raw_title):
@@ -81,7 +108,7 @@ def get_features(stng, vec_obj):
     return features, vectorizer
 
 # --------------------------------------------------
-def MNB_model_generate(X_train, X_test, y_train): # Multinomial Naive Bayes' Model
+def generate_model(X_train, X_test, y_train): # Multinomial Naive Bayes' Model
     """Train Naive Bayes model"""
     # Generate a blank multinomial naive bayes model
     naive_model = MultinomialNB()
@@ -90,6 +117,29 @@ def MNB_model_generate(X_train, X_test, y_train): # Multinomial Naive Bayes' Mod
     classifier = naive_model.fit(X_train, y_train)
     
     return classifier
+
+# --------------------------------------------------
+def make_confusion_matrix(MNB_prediction, y_test, accuracy_per): # Multinomial Naive Bayes' Model
+    """Plot confusion matrix"""
+    MNB_confusion = confusion_matrix(MNB_prediction, y_test)
+    plt.figure(figsize = (10,7))
+    sn.heatmap(MNB_confusion, annot=True)
+    plt.xlabel('Actual Class')
+    plt.ylabel('Predicted Class')
+    plt.title('Confusion Matrix \nAccuracy: {}%'.format(accuracy_per), size=14)
+    plt.show()
+ 
+# --------------------------------------------------
+def write_test_data(test_out, y_test, model_prediction, raw_data): # Multinomial Naive Bayes' Model
+    """Save data used for testing"""
+    with open(test_out,'a') as fh:
+            i = 0
+            fh.write('Actual\tPredicted\tTitle\n')
+            for ind, label in y_test.items():
+                pred = model_prediction[i]
+                tit = raw_data.title[ind]
+                fh.write('{}\t{}\t{}\n'.format(label, pred, tit))
+                i += 1
  
 # -------------------------------------------------- 
 def main():
@@ -99,57 +149,53 @@ def main():
     args = get_args()
     data_file = args.data
     pkl_file = args.out
+    test_out = args.test_out
+    split = args.test_split
     
+    # Check for data file
+    if not os.path.isfile(data_file):
+        die('Data file "{}" not found.'.format(data_file))
+    
+    # Read in labeled data
     raw_data = pd.read_csv(data_file, delimiter='\t', header=0)
     
+    # Extract title strings from data
     titles = []
     for i in range(raw_data.title.size):
         titles.append(clean_title(raw_data.title[i]))
 
+    # Extract data labels
     y = raw_data.label
     
-    titles_train, titles_test, y_train, y_test = train_test_split(titles, y,
-                                                                  test_size=0.2)
-    
+    # Split data between train and test
+    t_train, t_test, y_train, y_test = train_test_split(titles, y, test_size=split)
     print('Extracting features')
-    x_train, vectorizer = get_features(titles_train, None)
-    x_test, _ = get_features(titles_test, vectorizer)
+    x_train, vectorizer = get_features(t_train, None)
+    x_test, _ = get_features(t_test, vectorizer)
     
     print('Training model')
-    MNB_model = MNB_model_generate(x_train, x_test, y_train)
+    model = generate_model(x_train, x_test, y_train)
     
     print('Testing model')
-    MNB_prediction = MNB_model.predict(x_test)
+    model_prediction = model.predict(x_test)
+    model_accuracy = model.score(x_test, y_test)
+    accuracy_per = round(model_accuracy*1000)/10
+    print('Model accuracy: {}%'.format(accuracy_per))
     
-    MNB_confusion = confusion_matrix(MNB_prediction, y_test)
-    MNB_accuracy = MNB_model.score(x_test, y_test)
-    accuracy_per = round(MNB_accuracy*1000)/10
+    print('Assessing confusion matrix')
+    make_confusion_matrix(model_prediction, y_test, accuracy_per)    
     
-    plt.figure(figsize = (10,7))
-    sn.heatmap(MNB_confusion, annot=True)
-    plt.xlabel('Actual Class')
-    plt.ylabel('Predicted Class')
-    plt.title('Confusion Matrix \nAccuracy: {}%'.format(accuracy_per), size=14)
-    plt.show()
+    if os.path.isfile(test_out):
+        os.remove(test_out)
+        print('Removing previous test data file.')
     
     print('Saving test data.')
-    if os.path.isfile('test_data.txt'):
-        os.remove('test_data.txt')
-        
-    with open('test_data.txt','a') as fh:
-            i = 0
-            fh.write('Actual\tPredicted\tTitle\n')
-            for ind, label in y_test.items():
-                pred = MNB_prediction[i]
-                tit = raw_data.title[ind]
-                fh.write('{}\t{}\t{}\n'.format(label, pred, tit))
-                i += 1
-       
-    MNB_tuple = (MNB_model, x_test, y_test, MNB_accuracy, vectorizer)
+    write_test_data(test_out, y_test, model_prediction, raw_data)
     
     print('Saving pickle')
+    pickle_tuple = (model, x_test, y_test, model_accuracy, vectorizer)
     with open(pkl_file, 'wb') as file:
-        pickle.dump(MNB_tuple, file)
+        pickle.dump(pickle_tuple, file)
     
 # --------------------------------------------------
 if __name__ == '__main__':
